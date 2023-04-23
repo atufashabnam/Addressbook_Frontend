ab06231ad06c193966eedde004315cca76b0295a
Junaid Ahmed <junaid.ahmed@postnord.com>
2023-04-23 17:42:19 +0200

    address book html css

--- scripts/addressBook.js
+++ scripts/addressBook.js
@@ -1,25 +1,25 @@
-const form = document.getElementById('my-form');
-
-
-form.addEventListener('submit', (event)=> {
-    event.preventDefault(); 
-
-    const formData = new FormData(form);
-    console.log(formData.get( 'name'));
-    const  data =  Object.fromEntries(formData); 
-
-    fetch('http://localhost:8080/api/addresses', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
+function getAddress(){
+    fetch('http://localhost:8080/api/addresses')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => console.log("data from get"+JSON.stringify(data)))
     .catch(error => console.error(error));
+}
 
-});
+async function createAddress(){
+    const form = document.getElementById('my-form');
+    const formData = new FormData(form);
+    const  data =  Object.fromEntries(formData);
+    const createResult = await fetch('http://localhost:8080/api/addresses', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        });
+    console.log(json.stringify(createResult));
+}
+
+getAddress();
 
 
 
