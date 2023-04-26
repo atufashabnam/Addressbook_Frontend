dc98d8384ddab7e116305e4382c580096f316483
Junaid Ahmed <junaid.ahmed@postnord.com>
2023-04-26 11:18:19 +0200

    adding delte functionality

--- scripts/addressBook.js
+++ scripts/addressBook.js
@@ -1,22 +1,75 @@
 function getAddress(){
     fetch('http://localhost:8080/api/addresses')
     .then(response => response.json())
-    .then(data => console.log("data from get"+JSON.stringify(data)))
+    .then(data => {
+    const table = `
+      <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Address</th>
+          <th>Phone</th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${data.map(user => `
+          <tr id="${user.id}">
+            <td>${user.name}</td>
+            <td>${user.address}</td>
+            <td>${user.phone}</td>
+            <td>
+              <button onclick="updateRecord(${user.id})">Update</button>
+            </td>
+            <td>
+            <button onclick="deleteRecord(${user.id})">Delete</button>
+          </td>
+          </tr>
+        `).join('')}
+      </tbody></table>
+    `;
+    document.getElementById('my-table-container').innerHTML = table;
+    })
     .catch(error => console.error(error));
 }
 
 async function createAddress(){
+
     const form = document.getElementById('my-form');
     const formData = new FormData(form);
     const  data =  Object.fromEntries(formData);
-    const createResult = await fetch('http://localhost:8080/api/addresses', {
+    const options = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify(data)
-        });
-    console.log(json.stringify(createResult));
+        }
+    const createResult = await fetch('http://localhost:8080/api/addresses', options);
+        getAddress();
+    console.log(JSON.stringify(createResult));
+}
+
+function updateRecord(id){
+
+}
+
+function deleteRecord(id){
+
+
+        fetch(`http://localhost:8080/api/addresses?id=${id}`, { method: 'DELETE' })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      console.log(`Record with ID ${id} deleted successfully`);
+      // remove the row from the HTML table
+      getAddress();
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 }
 
 getAddress();
